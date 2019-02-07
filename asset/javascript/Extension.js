d40a3d11bc03932bc175c94a2ba17b84e942dfd0
Jean luc <you@example.com>
2019-02-07 12:29:37 +0100

    synchro

--- asset/javascript/Extension.js
+++ asset/javascript/Extension.js
@@ -2,11 +2,3 @@ Planck.Extension.ViewComponent = {};
 Planck.Extension.ViewComponent.View = {};
 
 Planck.Extension.ViewComponent.View.Component = {};
-Planck.Extension.ViewComponent.View.FormElement = {};
-
-Planck.Extension.ViewComponent.Module = {};
-Planck.Extension.ViewComponent.Model = {};
-Planck.Extension.ViewComponent.Model.Entity = {};
-Planck.Extension.ViewComponent.Model.Repository = {};
-
-
